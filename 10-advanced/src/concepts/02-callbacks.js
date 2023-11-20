caaadabe0a2f3715a51b52cd67a29c32747a48f5
cesargrivasm <cesarg.rivasm@gmail.com>
2023-11-20 15:54:26 -0400

    testing 2

--- 10-advanced/src/concepts/02-callbacks.js
+++ 10-advanced/src/concepts/02-callbacks.js
@@ -9,20 +9,45 @@ export const callbacksComponent = ( element ) => {
 
 //     console.log('callbacksComponent')
 // }
-const id = 'd86371f9f80b591f499df32';
-findHero( id , (hero) => {
-    element.innerHTML = hero.name;
+const id1 = 'd86371f9f80b591f499df3';
+const id2= '5d86371f25a058e5b1c8a65e';
 
+findHero( id1 , (error, hero1) => {
+
+    // element.innerHTML = hero?.name || 'no hay heroe';
+
+    if(error){
+        element.innerHTML = error;
+        return;
+    }
+
+    findHero( id2 , (error, hero2) => {
+
+        // element.innerHTML = hero?.name || 'no hay heroe';
+    
+        if(error){
+            element.innerHTML = error;
+            return;
+        }
+
+    element.innerHTML = `${ hero1.name}/ ${hero2.name}`;
+
+})
 });
 }
-
 /**
  * 
  * @param {string} id 
- * @param {(hero: Object) => void} callback 
+ * @param { (error: String|Null, hero: Object)=> void} callback 
  */
-const findHero = (id, callback) => {
+const findHero = (id,callback) => {
     const hero = heroes.find( hero => hero.id ==id );
 
-    callback( hero );
+    if(!hero){
+        callback(`Hero with id ${ id } not found.`);
+        return;
+    }
+
+    callback( null, hero );
 }
+
